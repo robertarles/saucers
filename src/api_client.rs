01c2ae43cad0d866a1cc0ec7caf37c8a42045a03
robert arles <robert.r.arles@disney.com>
2020-02-11 18:17:56 -0800

    moved to file modules format, adding -o handling

---
+++ src/api_client.rs
@@ -0,0 +1,105 @@
+use std::env;
+
+extern crate reqwest;
+extern crate serde;
+extern crate serde_json;
+
+
+static SAUCE_API_URL: &'static str = "https://saucelabs.com/rest/v1/";
+static API_STATUS_PATH: &'static str = "info/status";
+static GET_JOBS_PATH: &'static str = "/jobs";
+static GET_JOB_PATH: &'static str = "/jobs";
+
+/**
+ * returns the "healthcheck" api status from Saucelabs API
+ */
+pub fn get_api_status() -> serde_json::Value {
+
+    let resp = reqwest::blocking::get(&format!("{}{}", &SAUCE_API_URL, &API_STATUS_PATH)).unwrap();
+    //println!("API response: \n{:?}", resp.status());
+
+    let body = resp.text().unwrap();
+    let json: serde_json::Value = serde_json::from_str(&body).expect("JSON was not well-formatted");
+
+    json
+}
+
+/**
+ * return a list of jobs
+ */
+pub fn get_jobs(max: &str) -> serde_json::Value {
+    
+    let sauce_username = env::var("SAUCE_USERNAME").unwrap();
+    let sauce_access_key = env::var("SAUCE_ACCESS_KEY").unwrap();
+    
+    let jobs_params = format!("?limit={}&full=true", max);
+    let client = reqwest::blocking::Client::new();
+    let resp = client
+        .get(&format!("{}{}{}", &SAUCE_API_URL, &GET_JOBS_PATH, &jobs_params))
+        .basic_auth(sauce_username.clone(), Some(sauce_access_key.clone()))
+        .send().unwrap();
+    let body = resp.text().unwrap();
+    let json: serde_json::Value = serde_json::from_str(&body).expect("JSON was not well-formatted");
+
+    json
+}
+
+/**
+ * get a specific jobs details
+ */
+pub fn get_job(job_id: &str) -> serde_json::Value {
+    
+    let sauce_username = env::var("SAUCE_USERNAME").unwrap();
+    let sauce_access_key = env::var("SAUCE_ACCESS_KEY").unwrap();
+    
+    let client = reqwest::blocking::Client::new();
+    let resp = client
+        .get(&format!("{}{}/{}", &SAUCE_API_URL, &GET_JOB_PATH, &job_id))
+        .basic_auth(sauce_username.clone(), Some(sauce_access_key.clone()))
+        .send().unwrap();
+    let body = resp.text().unwrap();
+    let json: serde_json::Value = serde_json::from_str(&body).expect("JSON was not well-formatted");
+    
+    json
+}
+
+
+/**
+ * get a particular job asset file, valid file names:
+ * from their docs (I believe they are not correct/incomplete, at least the 'selenium host log?' is missing, there is another log file)
+ *  selenium-server.log, video.mp4, XXXXscreenshot.png (where XXXX is a number between 0000 and 9999), final_screenshot.png
+ */
+pub fn get_job_asset_file(job_id: &str, asset_filename: &str) -> String {
+    
+    let sauce_username = env::var("SAUCE_USERNAME").unwrap();
+    let sauce_access_key = env::var("SAUCE_ACCESS_KEY").unwrap();
+    
+    let client = reqwest::blocking::Client::new();
+    let resp = client
+        .get(&format!("{}{}/jobs/{}/assets/{}", &SAUCE_API_URL, &sauce_username, &job_id, &asset_filename))
+        .basic_auth(sauce_username.clone(), Some(sauce_access_key.clone()))
+        .send().unwrap();
+    let body = resp.text().unwrap();
+    
+    body
+}
+
+/**
+ * get the asset list associated with a particular job ID
+ */
+pub fn get_job_asset_list(job_id: &str) -> serde_json::Value{
+    
+    // apiURL+"/"+username+"/jobs/"+jobID+"/assets
+    let sauce_username = env::var("SAUCE_USERNAME").unwrap();
+    let sauce_access_key = env::var("SAUCE_ACCESS_KEY").unwrap();
+    
+    let client = reqwest::blocking::Client::new();
+    let resp = client
+        .get(&format!("{}{}/jobs/{}/assets", &SAUCE_API_URL, &sauce_username, &job_id))
+        .basic_auth(sauce_username.clone(), Some(sauce_access_key.clone()))
+        .send().unwrap();
+    let body = resp.text().unwrap();
+    let json: serde_json::Value = serde_json::from_str(&body).expect("JSON was not well-formatted");
+    
+    json
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod api_client;
+use std::collections::HashMap;
 
 extern crate clap;
 use clap::{Arg, App, SubCommand};
@@ -18,6 +19,12 @@ fn main() {
                 .takes_value(true)
                 .required(true)
                 .help("job ID")
+            )
+            .arg(Arg::with_name("formatted")
+                .short("o")
+                .takes_value(true)
+                .required(false)
+                .help("Request output in columnar format, requires field names to include in table (e.g. \"id,status,passed\")")
             )
         )
         .subcommand(SubCommand::with_name("jobs")
@@ -58,35 +65,114 @@ fn main() {
     
     // select the subcommand to run
     match matches.subcommand_name() {
-        Some("apistatus") => api_client::get_api_status(),
+        Some("apistatus") => {
+            api_client::get_api_status();
+        },
         Some("job") => {
             let job_args = matches.subcommand_matches("job").unwrap();
-            let job_id = job_args.value_of("id").unwrap(); // required arg, safe to simply unwrap
-            api_client::get_job(&job_id[..])
+            call_get_job(job_args);
         },
         Some("assetfile") => {
             let job_args = matches.subcommand_matches("assetfile").unwrap();
-            let job_id = job_args.value_of("id").unwrap(); // required arg, safe to simply unwrap
-            let asset_filename = job_args.value_of("filename").unwrap(); // required arg, safe to simply unwrap
-            api_client::get_job_asset_file(&job_id[..], &asset_filename[..])
+            call_get_job_asset_file(job_args);
         },
         Some("assetlist") => {
             let job_args = matches.subcommand_matches("assetlist").unwrap();
-            let job_id = job_args.value_of("id").unwrap(); // required arg, safe to simply unwrap
-            api_client::get_job_asset_list(&job_id[..])
+            call_get_job_asset_list(job_args);
         },
         Some("jobs") => {
-            // set a default 'max'
-            let mut max = String::from("10");
             // get the args from the subcommand, if max is provided, use its val
-            let jobs_args = matches.subcommand_matches("jobs").unwrap();
-            if jobs_args.is_present("max") {
-                max = jobs_args.value_of("max").unwrap().to_string();
-            };
-            api_client::get_jobs(&max[..])
+            let job_args = matches.subcommand_matches("jobs").unwrap();
+            call_get_jobs(job_args);
         },
         None => println!("No subcommand was used"),
         _ => println!("Subcommand not implemented!"),
     }
 
+    fn call_get_job(job_args: &clap::ArgMatches){
+        let job_id = job_args.value_of("id").unwrap(); // required arg, safe to simply unwrap
+        let json_response = api_client::get_job(&job_id[..]);
+        if job_args.is_present("formatted") {
+            let format_fields_string = job_args.value_of("formatted").unwrap();
+            let format_fields_string_cleaned = format_fields_string.replace(" ","");
+            let format_fields = format_fields_string_cleaned.split(",").collect();
+            print_formatted(json_response, format_fields);
+        } else {
+            println!("{}", json_response);
+        }
+    }
+
+    fn print_formatted(json: serde_json::Value, field_names: Vec<&str>) {
+        //println!("[DEBUG] passed to print_formatted(): {}\n{:#?}", json, field_names);
+        let mut field_vals_vec: std::vec::Vec<std::collections::HashMap<String,String>> = vec![];
+        let mut field_lens: std::collections::HashMap<String,usize> = HashMap::new();
+        for field in &field_names{
+            field_lens.insert(field.to_string(), field.len());
+        }
+        if json.is_array() {
+            for json_item in json.as_array().unwrap().iter(){
+                println!("[DEBUG] json_item {:#?}", json_item);
+                for field_name in &field_names {
+                    println!("{:#?}", field_name);
+                    //if json_item.as_object().unwrap().is_present(field_name){
+                        // put field_name value in VALUES map
+                        // /let mut fields_map: std::collections::HashMap<String,String> = HashMap::new();
+                        // if len > field_lens[field_name] then field_lens[fields] = field_name.len
+                    //}
+                }
+                // place VALUES map on fields_map
+            }
+        } else {   // else
+            for field_name in &field_names {
+                // println!("{:#?}", field_name);
+                let mut fields_map: std::collections::HashMap<String,String> = HashMap::new();
+                let json_object = json.as_object().unwrap();
+                if json_object.contains_key(&field_name.to_string()) {
+                    // println!("[DEBUG] json has key {}", &field_name.to_string());
+                    fields_map.insert(field_name.to_string(), json_object.get(&field_name.to_string()).unwrap().to_string());
+                    field_vals_vec.push(fields_map);
+                    let current_val_len = json_object.get(&field_name.to_string()).unwrap().to_string().len();
+                    let record_len = field_lens.get(&field_name.to_string()).unwrap();
+                    if &current_val_len > record_len { 
+                        field_lens.insert(field_name.to_string(), current_val_len); 
+                    }
+                }
+
+            }
+            println!("[DEBUG] field vals stored {:#?}", field_vals_vec);
+            println!("[DEBUG] field len records {:#?}", field_lens);
+            // place VALUES map on fields_map
+        }
+        // print table
+        // for field_name in field_names
+            //print("{:field_lens[field_name]+1}",field_name)
+        // println!("\n{:repeated sum of field name + num of field names}", "-")
+        // for each entry in fields_map
+            // for each field_name in field_names
+                // print("{:+repeated ${field_lens[field_name]-val.len} spaces}", val)
+            //println!()
+    }
+
+    fn call_get_job_asset_file(job_args: &clap::ArgMatches){
+        let job_id = job_args.value_of("id").unwrap(); // required arg, safe to simply unwrap
+        let asset_filename = job_args.value_of("filename").unwrap(); // required arg, safe to simply unwrap
+        let text_response = api_client::get_job_asset_file(&job_id[..], &asset_filename[..]);
+        println!("{}", text_response);
+    }
+
+    fn call_get_job_asset_list(job_args: &clap::ArgMatches){
+        let job_id = job_args.value_of("id").unwrap(); // required arg, safe to simply unwrap
+        let json_response = api_client::get_job_asset_list(&job_id[..]);
+        println!("{}", json_response);
+    }
+
+    fn call_get_jobs(job_args: &clap::ArgMatches){
+        // set a default 'max'
+        let mut max = String::from("10");
+        if job_args.is_present("max") {
+            max = job_args.value_of("max").unwrap().to_string();
+        };
+        let json_response = api_client::get_jobs(&max[..]);
+        println!("{}", json_response);
+    }
 }
